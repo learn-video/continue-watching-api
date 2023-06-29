1a8f84371222dba7000cc9f0cb3bd05a581ad81b
Maurício Antunes <mauricio.abreua@gmail.com>
2023-06-29 20:07:40 -0300

    Add Fetch function

--- position/position.go
+++ position/position.go
@@ -2,10 +2,16 @@ package position
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
+)
+
+var (
+	ErrNotFound = errors.New("no position tracked")
 )
 
 type Position struct {
@@ -15,7 +21,23 @@ type Position struct {
 }
 
 func Record(r *redis.Client, userID, videoID string, position int) error {
-	ctx := context.TODO()
 	key := fmt.Sprintf("%s_%s", userID, videoID)
-	return r.Set(ctx, key, position, 1*time.Minute).Err()
+	return r.Set(context.TODO(), key, position, 1*time.Minute).Err()
+}
+
+func Fetch(r *redis.Client, userID, videoID string) (int, error) {
+	key := fmt.Sprintf("%s_%s", userID, videoID)
+	val, err := r.Get(context.TODO(), key).Result()
+	if err == redis.Nil {
+		return 0, ErrNotFound
+	} else if err != nil {
+		return 0, err
+	}
+
+	pos, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, err
+	}
+
+	return pos, nil
 }
--- position/position_test.go
+++ position/position_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-redis/redismock/v9"
 	"github.com/learn-video/continue-watching-api/position"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -29,10 +30,52 @@ func TestRecordRedisError(t *testing.T) {
 	userID := "bda031c0-4e7d-493a-92ba-6fc1eb3e6216"
 	videoID := "a1fbd2af-ab5e-44ac-9e5d-1a24051f89cf"
 	key := fmt.Sprintf("%s_%s", userID, videoID)
-
 	mock.ExpectSet(key, 1, 1*time.Minute).SetErr(errors.New("failed to set key"))
 
 	err := position.Record(db, userID, videoID, 1)
+
 	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestFetchOK(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	userID := "bda031c0-4e7d-493a-92ba-6fc1eb3e6216"
+	videoID := "a1fbd2af-ab5e-44ac-9e5d-1a24051f89cf"
+	key := fmt.Sprintf("%s_%s", userID, videoID)
+	mock.ExpectGet(key).SetVal("1")
+
+	pos, err := position.Fetch(db, userID, videoID)
+
+	assert.Nil(t, err)
+	assert.Equal(t, pos, 1)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestFetchPositionNotFound(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	userID := "bda031c0-4e7d-493a-92ba-6fc1eb3e6216"
+	videoID := "a1fbd2af-ab5e-44ac-9e5d-1a24051f89cf"
+	key := fmt.Sprintf("%s_%s", userID, videoID)
+	mock.ExpectGet(key).SetErr(redis.Nil)
+
+	pos, err := position.Fetch(db, userID, videoID)
+
+	assert.Equal(t, err, position.ErrNotFound)
+	assert.Equal(t, pos, 0)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestFetchRedisError(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	userID := "bda031c0-4e7d-493a-92ba-6fc1eb3e6216"
+	videoID := "a1fbd2af-ab5e-44ac-9e5d-1a24051f89cf"
+	key := fmt.Sprintf("%s_%s", userID, videoID)
+	mock.ExpectGet(key).SetErr(errors.New("failed to get key"))
+
+	pos, err := position.Fetch(db, userID, videoID)
+
+	assert.Equal(t, err, errors.New("failed to get key"))
+	assert.Equal(t, pos, 0)
 	assert.Nil(t, mock.ExpectationsWereMet())
 }
